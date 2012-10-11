1f1020a31168149654fcc6d8209171046826ecb3
Tyler Butler <tyler@tylerbutler.com>
2012-10-11 12:40:13 -0700

    Bump version.

--- tokenauth/__init__.py
+++ tokenauth/__init__.py
@@ -2,4 +2,4 @@
 """A set of models, decorators, and a custom authentication backend that can be used to
 add simple token-based authentication to a Django project."""
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
