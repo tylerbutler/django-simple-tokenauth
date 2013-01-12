75c09178050853e08cdb406e24efe62a9446c3ca
Tyler Butler <tyler@tylerbutler.com>
2013-01-12 14:00:46 -0800

    Support passing access_token in POST body in addition to query string.

--- tokenauth/decorators.py
+++ tokenauth/decorators.py
@@ -13,27 +13,26 @@ def token_required(view_func):
     """
     @wraps(view_func, assigned=available_attrs(view_func))
     def _wrapped_view(request, *args, **kwargs):
-        if request.method == 'GET':
-            qsp = parse_qs(request.META['QUERY_STRING'], keep_blank_values=True)
-            qsp = CaseInsensitiveDict(qsp)
-            kw = CaseInsensitiveDict(kwargs)
-            try:
-                provided_token = qsp['access_token'][0]
-                if 'id' in kw.keys():
-                    resource_id = kw['id']
-                elif 'file_id' in kw.keys():
-                    resource_id = kw['file_id']
-                elif 'resource_id' in kw.keys():
-                    resource_id = kw['resource_id']
-                else:
-                    return HttpResponseForbidden()
-            except KeyError:
-                return HttpResponseForbidden()
-
-            user = authenticate(access_token=provided_token, resource_id=resource_id)
-            if user:
-                login(request, user)
-                return view_func(request, *args, **kwargs)
+        qsp = CaseInsensitiveDict(request.REQUEST)
+        kw = CaseInsensitiveDict(kwargs)
+        try:
+            provided_token = qsp['access_token']
+        except KeyError:
             return HttpResponseForbidden()
+
+        if 'id' in kw:
+            resource_id = kw['id']
+        elif 'file_id' in kw:
+            resource_id = kw['file_id']
+        elif 'resource_id' in kw:
+            resource_id = kw['resource_id']
+        else:
+            return HttpResponseForbidden()
+
+        user = authenticate(access_token=provided_token, resource_id=resource_id)
+        if user:
+            login(request, user)
+            return view_func(request, *args, **kwargs)
+        return HttpResponseForbidden()
 
     return _wrapped_view
